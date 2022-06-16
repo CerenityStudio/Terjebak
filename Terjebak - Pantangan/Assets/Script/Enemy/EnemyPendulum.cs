170818c6657403808f8b4fc61056e8f3bbde2d6a
Murran <nfmurran@staff.sfcerenity.org>
2022-06-16 19:21:22 +0700

    Add Pendulum Trap

---
+++ Terjebak - Pantangan/Assets/Script/Enemy/EnemyPendulum.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPendulum : MonoBehaviour
+{
+    #region Public Variables
+    public float leftPushRange, rightPushRange;
+    public float velocityThreshold;
+    #endregion
+
+    #region Private Variable
+    private Rigidbody2D rb;
+    #endregion
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.angularVelocity = velocityThreshold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Push();
+    }
+
+    public void Push()
+    {
+        if (transform.rotation.z > 0 && transform.rotation.z < rightPushRange && (rb.angularVelocity > 0) && rb.angularVelocity < velocityThreshold)
+        {
+            rb.angularVelocity = velocityThreshold;
+        }
+        else if (transform.rotation.z < 0 && transform.rotation.z > leftPushRange && (rb.angularVelocity < 0) && rb.angularVelocity > velocityThreshold * -1)
+        {
+            rb.angularVelocity = velocityThreshold * -1;
+        }
+    }
+}
